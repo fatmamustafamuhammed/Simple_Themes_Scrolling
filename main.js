01f6390ccbb49dbe5fa989170c6d0f87096dc3db
fatmamustafamuhammed <fmomen738@gmail.com>
2025-08-17 17:45:44 +0300

    Enhance Project Script & Style

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
+let divClasses = document.querySelectorAll('.div');
+
+divClasses.forEach((div) => {
+    div.onclick = function () {
+        setBgColor(div.id); // value of div.id will be the background-color of the body 
+    }
+});
+
 if (localStorage.length > 0) {
-    document.body.style.background = localStorage.color;
+    document.body.style.background = localStorage.bgColor;
 }
 
-function setColor(color) {
-    localStorage.setItem('color', color)
-    document.body.style.background = color;
+function setBgColor(bgColor) {
+    localStorage.setItem('bgColor', bgColor)
+    document.body.style.background = bgColor;
 }
 
-// 
+//// Scroll to Top Button
 
 let btn = document.getElementById("btn");
 
